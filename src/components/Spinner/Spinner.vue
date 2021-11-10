943d180f77aaac01cd39e2d8edbe778b50b6b619
Thomas van der Veen <thomas@owow.io>
2021-11-10 13:06:26 +0100

    Spinner component

---
+++ src/components/Spinner/Spinner.vue
@@ -0,0 +1,36 @@
+<template>
+    <div :class="classes" />
+</template>
+
+<script lang="ts">
+export const types = ['border', 'grow'] as const;
+
+type Type = typeof types[number];
+</script>
+
+<script lang="ts" setup>
+import useSize, {sizeProps} from '../../composables/useSize';
+import useVariant, {variantProps} from '../../composables/useVariant';
+import {PropType} from 'vue';
+import useClasses from '../../composables/useClasses';
+import useStringTemplate from '../../composables/useStringTemplate';
+
+const props = defineProps({
+    ...sizeProps,
+    type: {
+        type: String as PropType<Type>,
+        default: 'border',
+        validator: (t: Type) => types.includes(t),
+    },
+    ...variantProps,
+});
+
+const spinnerClass = useStringTemplate('spinner-{0}', props.type).templatedString;
+
+const {classes} = useClasses([
+    useSize(props.size, `${spinnerClass.value}-{0}`).sizeClass.value,
+    spinnerClass.value,
+    useVariant(props.variant, 'text-{0}').variantClass.value,
+]);
+</script>
+
