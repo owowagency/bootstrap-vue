67f1f682ebfa1b7ab352842ecf11bf96dafc859e
Thomas van der Veen <thomas11juni@hotmail.com>
2021-11-15 13:25:53 +0100

    Remove

--- src/components/FormPills/FormPills.vue
+++ src/components/FormPills/FormPills.vue
@@ -29,8 +29,8 @@
                     v-model="value"
                     class="form-pill-input w-100"
                     type="text"
-                    @keydown="keydown($event)"
-                    @paste="paste($event)"
+                    @keydown="keydown"
+                    @paste="paste"
                 >
             </li>
         </ul>
