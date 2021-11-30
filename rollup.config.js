094237082cfb9425fb8d9f84fd6168506358b618
Thomas van der Veen <thomas@owow.io>
2021-11-30 13:11:41 +0100

    fix(rollup): just release me :(

--- rollup.config.js
+++ rollup.config.js
@@ -25,6 +25,7 @@ export default {
     input: 'src/index.ts',
     output: [
         createEntry({format: 'es', file: pkg.main}),
+        // Just release me :(
         // createEntry({format: 'iife', file: pkg.browser}),
         // createEntry({format: 'cjs', file: pkg.main}),
     ],
