20a6990635a62baee50e85325f346a13ede905e5
Albert Pratomo <albertpratomo21@gmail.com>
2022-03-01 10:51:01 +0100

    feat(Confirm): add more props, button-ok and button-cancel slots

--- src/components/Confirm/Confirm.vue
+++ src/components/Confirm/Confirm.vue
@@ -1,24 +1,81 @@
 <template>
     <Modal
-        title="Confirm"
-        body="You are about to do something that requires confirmation. Do you wish to continue?"
+        ref="modal"
+        :title="title"
     >
-        <template #footer>
-            <Btn
-                data-bs-dismiss="modal"
-                variant="light"
-            >
-                Cancel
-            </Btn>
+        <slot>
+            {{ body }}
+        </slot>
 
-            <Btn variant="danger">
-                Confirm
-            </Btn>
+        <template #footer>
+            <slot name="button-cancel">
+                <Button
+                    :outline="cancelOutline"
+                    :variant="cancelVariant"
+                    @click="hide"
+                >
+                    {{ cancelText }}
+                </Button>
+            </slot>
+
+            <slot name="button-ok">
+                <Button
+                    :variant="okVariant"
+                    @click="ok"
+                >
+                    {{ okText }}
+                </Button>
+            </slot>
         </template>
     </Modal>
 </template>
 
-<script lang="ts" setup>
-import Btn from '@/components/Button';
+<script lang="ts">
+import {ComponentPublicInstance, ref} from 'vue';
+import Button from '@/components/Button';
 import Modal from '@/components/Modal';
+import {variantProp} from '@/composables/useVariant';
+</script>
+
+<script lang="ts" setup>
+defineProps({
+    body: {
+        type: String,
+        default: 'Are you sure?',
+    },
+    cancelOutline: {
+        type: Boolean,
+        default: false,
+    },
+    cancelText: {
+        type: String,
+        default: 'No',
+    },
+    cancelVariant: variantProp('light'),
+    okText: {
+        type: String,
+        default: 'Yes',
+    },
+    okVariant: variantProp('primary'),
+    title: {
+        type: String,
+        default: 'Confirmation',
+    },
+});
+
+const emit = defineEmits<{(e: 'confirmed'): void}>();
+
+const ok = () => {
+    emit('confirmed');
+
+    hide();
+};
+
+const modal = ref<ComponentPublicInstance<typeof Modal>>();
+
+const hide = () => modal.value.bsModal.hide();
+
+const show = () => modal.value.bsModal.show();
+
+defineExpose({hide, show});
 </script>
