57359227874a6d5b9dca57d6e3712851091c6353
Thomas van der Veen <thomas@owow.io>
2022-04-05 11:02:53 +0200

    Responsive align and justify

--- src/components/Row/type.ts
+++ src/components/Row/type.ts
@@ -1,22 +1,36 @@
-import {Prop} from 'vue';
+import {Prop, PropType} from 'vue';
 
 const aligns = ['center', 'end', 'start'] as const;
 
 type Align = typeof aligns[number];
 
-const justifies = ['around', 'between', 'center', 'end', 'evenly', 'start'] as const;
-
-type Justify = typeof justifies[number];
+const alignProp: Prop<Align> = {
+    type: String as PropType<Align>,
+    default: undefined,
+    validator: (a: Align) => aligns.includes(a),
+};
 
 const gutterProp: Prop<number | string> = {
     type: [Number, String],
     default: undefined,
 };
 
+const justifies = ['around', 'between', 'center', 'end', 'evenly', 'start'] as const;
+
+type Justify = typeof justifies[number];
+
+const justifyProp: Prop<Justify> = {
+    type: String as PropType<Justify>,
+    default: undefined,
+    validator: (j: Justify) => justifies.includes(j),
+};
+
 export {
-    aligns,
     Align,
-    justifies,
-    Justify,
+    alignProp,
+    aligns,
     gutterProp,
+    Justify,
+    justifies,
+    justifyProp,
 };
