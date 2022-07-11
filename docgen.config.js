42c02f6478f7d806f8889f696b293e14cddd9f63
Sjoertjuh <sjoert2001@gmail.com>
2022-07-11 17:11:51 +0200

    Removed unused features

--- docgen.config.js
+++ docgen.config.js
@@ -1,15 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
-const temp = (
-    renderedUsage, // props, events, methods and slots documentation rendered
-    doc, // the object returned by vue-docgen-api
-    config, // the local config, useful to know the context
-    fileName, // the name of the current file in the doc (to explain how to import it)
-    requiresMd, // a list of all the documentation files
-    // attached to the component documented. It includes documentation of subcomponents
-    {isSubComponent, hasSubComponents}, // are we documenting
-) => {
+const temp = (renderedUsage, doc) => {
     const {displayName, description, docsBlocks} = doc;
 
     return `
@@ -31,7 +23,6 @@ module.exports = {
     components: '**/Accordion*.vue',
     defaultExamples: true,
     docsBranch: 'master',
-    // docsFolder: 'src/components',
     docsRepo: 'owowagency/bootstrap-vue',
     editLinkLabel: 'Edit on GitHub',
     getDestFile: (file, config) => {
@@ -40,14 +31,6 @@ module.exports = {
     getDocFileName: (componentPath) => componentPath.replace(/\.vue$/, '.md'), // specify the name of the input md file
     outDir: 'src/components',
     templates: {
-        // // global component template wrapping all others see #templates
-        // component: () => '',
-        // events: () => '',
-        // methods: () => '',
-        // props: () => '',
-        // slots: () => '',
-        // static template to display as a tag if component is functional
-        // functionalTag: '**functional**',
         component: temp,
     },
 };
