fec1fd475688463a751a11e101aed8fd82e2e52b
Sjoert <63722509+Sjoertjuh@users.noreply.github.com>
2023-11-13 16:05:06 +0100

    add required option to formControl props

--- src/composables/useFormControl/index.ts
+++ src/composables/useFormControl/index.ts
@@ -28,6 +28,10 @@ export const formControlProps = {
         type: Boolean,
         default: false,
     },
+    required: {
+        type: Boolean,
+        default: false,
+    },
     ...sizeProps,
     type: {
         type: String as PropType<Type>,
