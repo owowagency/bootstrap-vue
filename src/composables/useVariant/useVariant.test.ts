643ad64411776f17ee829e56da77087c2f5a7caa
Sjoertjuh <63722509+Sjoertjuh@users.noreply.github.com>
2023-11-22 11:34:06 +0100

    fix(useVariant): allow custom variants

--- src/composables/useVariant/useVariant.test.ts
+++ src/composables/useVariant/useVariant.test.ts
@@ -1,29 +1,5 @@
 import useVariant, {variantProp, variantProps, variants} from '.';
 
-describe('variantProps', () => {
-    describe('variant', () => {
-        variants.forEach((s) => {
-            it(`passes validation with ${s}`, () => {
-                const result = variantProps.variant.validator(s);
-
-                expect(result).toBe(true);
-            });
-        });
-
-        it('passes validation with empty value', () => {
-            const result = variantProps.variant.validator(undefined);
-
-            expect(result).toBe(true);
-        });
-
-        it('passes validation with a non variant', () => {
-            const result = variantProps.variant.validator('non-variant');
-
-            expect(result).toBe(false);
-        });
-    });
-});
-
 describe('variantProp', () => {
     it('sets the default value', () => {
         const prop = variantProp('');
